59aaaeda615e8c74dfc178d7a1d14fcca151ef50
sn1p3r46 <kingokongo46@gmail.com>
2014-11-24 18:38:44 +0100

    added handlerFile.py (scheleton)

---
+++ handlerFile.py
@@ -0,0 +1,19 @@
+import scipy as sp
+import numpy as np
+
+#Give Me Matrix From File
+def gmmff():
+
+
+#Print Matrix on File
+def pmof():
+
+
+#Generate Random Integer Square Matrix
+def grism(Size):
+    return np.random.randint(-1000, high=1000, size=(Size,Size))
+
+
+#Generate Random Real Square Matrix
+def grrsm():
+    return 0
