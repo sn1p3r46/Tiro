1e40130d364ebb994e77c3012d12860a10332bb5
sn1p3r46 <kingokongo46@gmail.com>
2014-11-25 02:35:23 +0100

    Added IOFileSupport creating folders to work with files

--- handlerFile.py
+++ handlerFile.py
@@ -1,12 +1,32 @@
 import scipy as sp
 import numpy as np
+from os.path import expanduser
+import os
+
+workspace = "/Tiro/IOFiles/"
+home = expanduser("~") #home variable contains the home folder's absolute path
+
+def createWorkSpace():
+    if( not os.path.isdir(home+workspace)):
+        print "Path does not exists"
+        os.makedirs(home+workspace)
+        print 'Don\'t warry I\' ve just created it'
+    else:
+        print "File Folder is OK!"
+
 
 #Give Me Matrix From File
 def gmmff():
+    return 0
 
 
 #Print Matrix on File
-def pmof():
+def pmof(Matrix,filename=None):
+    if (filename is None):
+        #s
+        filename = time.strftime("%y.%m.%d-%H.%M.%S")+".out"
+    np.savetxt(home+workspace+filename,Matrix,fmt='%17.10f')
+    return 0
 
 
 #Generate Random Integer Square Matrix
@@ -17,3 +37,7 @@ def grism(Size):
 #Generate Random Real Square Matrix
 def grrsm():
     return 0
+
+
+#############TO RUN ##############
+createWorkSpace()
