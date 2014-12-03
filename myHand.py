90160e78c57e1fcd0adb800ed046415bb6a88a1c
sn1p3r46 <kingokongo46@gmail.com>
2014-12-03 15:37:40 +0100

    Added csr0 and cs1 Complex Numbers Square Root

--- myHand.py
+++ myHand.py
@@ -5,15 +5,29 @@ import os
 from handlerFile import *
 from os.path import expanduser
 
+
 def main():
     print "\n###########################"
 
+
 def applySchur(SourceMatrix):
-    T,Z = sp.linalg.schur(SourceMatrix)
-    #pmof(SourceMatrix,"Source.out")
-    pmof(T,"Triangular.out")
-    pmof(Z,"Unitary.out")
+    T, Z = sp.linalg.schur(SourceMatrix)
+#   pmof(SourceMatrix,"Source.out")
+    pmof(T, "Triangular.out")
+    pmof(Z, "Unitary.out")
     return 0
+
+
+def csr0(complex_n):
+    s = sp.sqrt(sp.square(complex_n.real)+sp.square(complex_n.imag))
+    angle = np.angle(complex_n)
+    return sp.sqrt(s)*(complex((sp.cos(angle*0.5)), (sp.sin(angle*0.5))))
+
+def csr1(complex_n):
+    CMP0 = (sp.sqrt(sp.square(complex_n.real)+sp.square(complex_n.imag)))/2
+    CMP1 = (complex_n.real/2)
+    return complex(sp.sqrt(CMP0+CMP1),sp.sign(complex_n.imag)*(sp.sqrt(CMP0-CMP1)))
+
 
 
 if __name__ == "__main__":
