86cf569470f521f4ddd23646f5cd187f6df98b67
sn1p3r46 <kingokongo46@gmail.com>
2014-12-04 17:42:52 +0100

    added csr2.. it seems does not work properly

--- myHand.py
+++ myHand.py
@@ -19,19 +19,26 @@ def applySchur(SourceMatrix):
 
 
 def csr0(complex_n):
-    s = sp.sqrt(sp.square(complex_n.real)+sp.square(complex_n.imag))
+    s = sp.sqrt(sp.square(complex_n.real)+sp.square(complex_n.imag)) # np.absolute()
     angle = np.angle(complex_n)
     return sp.sqrt(s)*(complex((sp.cos(angle*0.5)), (sp.sin(angle*0.5))))
 
+
 def csr1(complex_n):
-    CMP0 = (sp.sqrt(sp.square(complex_n.real)+sp.square(complex_n.imag)))/2
+    CMP0 = (sp.sqrt(sp.square(complex_n.real)+sp.square(complex_n.imag)))/2 #np.absolute()/2
     CMP1 = (complex_n.real/2)
     return complex(sp.sqrt(CMP0+CMP1),sp.sign(complex_n.imag)*(sp.sqrt(CMP0-CMP1)))
 
 
+def csr2(complex_n):
+    t = sp.sqrt((sp.absolute(complex_n.real)+(sp.sqrt(sp.square(complex_n.real)+sp.square(complex_n.imag))))/2)
+    if(complex_n.real >= 0):
+        return complex(t,(complex_n.imag/(2*t)))
+    else:
+        return complex((complex_n.imag/(2*t)),t)
 
 if __name__ == "__main__":
-    a=grrsm(7)
-    applySchur(gmff("Source.out"))
+    h=grrsm(7)
+    #applySchur(gmff("Source.out"))
 
     print "\nImported\n"
