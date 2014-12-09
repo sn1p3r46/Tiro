f18083915f9cb58fb092823d4de1059ff9038ccf
sn1p3r46 <kingokongo46@gmail.com>
2014-12-09 05:36:00 +0100

    nightly updates

--- myHand.py
+++ myHand.py
@@ -28,7 +28,7 @@ def findBlocks0(matrix):
             i+=1
         else:
             bb[j] = eb[j] = i
-            dims[i] = 1
+            dims[j] = 1
             wb[i] = j
         i+=1
         j+=1
@@ -36,21 +36,51 @@ def findBlocks0(matrix):
     print eb; print"\n"
     print wb; print"\n"
     print dims; print"\n"
-    return bb,eb,dims,wb,i,j
+    return bb,eb,dims,wb,i,j #i,wb dims maybe are useless
 
 
-def getBlocks0(matrix,j):
-    a = sp.ndarray(j)
+def getBlocks0(matrix):
+    #add findBlocks0() return variables here!
+    bb,eb,dims,wb,i,j = findBlocks0(matrix)
+    #a = sp.ndarray(j)
+    b = []
     for i in range(0,j):
-        print matrix[bb[i]:eb[i]+1,bb[i]:eb[i]+1] #sp.append may be high cost operation
-    return a
+        b.append(matrix[bb[i]:eb[i]+1,bb[i]:eb[i]+1]) #sp.append may be high cost operation
+    return b
 
+#much more simple! It Works!!! needs only matrix j(block number) and
+#ordered dims of the blockso
+
+def getBlocks1(matrix):
+    bb,eb,dims,wb,i,j = findBlocks0(matrix)
+    b = []
+    k=0
+    for i in range (0,j):
+        b.append(matrix[k:k+dims[i],k:k+dims[i]])
+        k += dims[i]
+        print k
+        print dims[i]; print '<--Dims'
+
+    return b
+
+
+#get eigenvalues perche' prendo quelli con parte immaginaria > 0 ?
+
+def gev(matrix):
+    x = (matrix[0,0]+matrix[1,1])/2
+    y = sp.sqrt(-sp.square(matrix[0,0]-matrix[1,1])-4*matrix[1,0]*matrix[0,1])/2
+    return complex(x,y)
+
+
+
+# nroot = r^n(cos(ang*n) + i sin(ang*n))
 
 def csr0(complex_n):
     s = sp.sqrt(sp.square(complex_n.real)+sp.square(complex_n.imag)) # np.absolute()
     angle = np.angle(complex_n)
     return sp.sqrt(s)*(complex((sp.cos(angle*0.5)), (sp.sin(angle*0.5))))
 
+# From http://www.mathpropress.com/stan/bibliography/complexSquareRoot.pdf
 
 def csr1(complex_n):
     CMP0 = (sp.sqrt(sp.square(complex_n.real)+sp.square(complex_n.imag)))/2 #np.absolute()/2
@@ -58,12 +88,31 @@ def csr1(complex_n):
     return complex(sp.sqrt(CMP0+CMP1),sp.sign(complex_n.imag)*(sp.sqrt(CMP0-CMP1)))
 
 
+# From the book (Seems does not work properly, descover why...)
+
 def csr2(complex_n):
     t = sp.sqrt((sp.absolute(complex_n.real)+(sp.sqrt(sp.square(complex_n.real)+sp.square(complex_n.imag))))/2)
     if(complex_n.real >= 0):
         return complex(t,(complex_n.imag/(2*t)))
     else:
         return complex((complex_n.imag/(2*t)),t)
+
+# De Moivre
+    # -- square roots of a complex number are 2; n-roots of a square number are n
+    # -- s^(1/n)* [cos((ang/2)+(2pi*k)/n) +i sin((ang/2)+(2pi*k)/n)] with k = [0...n-1]
+
+def csr3(complex_n):
+    ang = sp.angle(complex_n) # sp.arctan(a.imag/a.real) why it does not work?!?!
+    r = sp.sqrt(sp.square(complex_n.real)+sp.square(complex_n.imag))
+    if (sp.cos(ang/2)>=0):
+        return sp.sqrt(r)*(complex(sp.cos(ang/2),sp.sin(ang/2)))
+    else:
+        return sp.sqrt(r)*(complex(sp.cos((ang/2)+sp.pi),sp.sin((ang/2)+sp.pi)))
+
+    #r1 = sp.sqrt(r)*(complex(sp.cos(ang/2),sp.sin(ang/2)))
+    #r2 = sp.sqrt(r)*(complex(sp.cos((ang/2)+sp.pi),sp.sin((ang/2)+sp.pi)))
+    #return r1,r2
+
 
 if __name__ == "__main__":
     h = grism(10)
