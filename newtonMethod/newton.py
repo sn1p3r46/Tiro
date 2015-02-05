1daf23dc12f98ed8a68b3071cd66d360595635cc
sn1p3r46 <kingokongo46@gmail.com>
2015-02-05 21:24:48 +0100

    Implemented Newton method and it's Variants

---
+++ newtonMethod/newton.py
@@ -0,0 +1,65 @@
+import scipy as sp
+import numpy as np
+
+def newton(M):
+    A = np.copy(M)
+    X = np.copy(M)
+    for i in range(0,30):
+        X = 0.5*(X + np.linalg.inv(X).dot(A))
+
+    return X
+
+def DB(M):
+    Xk = np.copy(M)
+    Yk = np.eye(M.shape[0])
+    for i in range(0,30):
+        Xk1 = (Xk + np.linalg.inv(Yk))/2
+        Yk1 = (Yk + np.linalg.inv(Xk))/2
+        Xk = Xk1
+        Yk = Yk1
+    return Xk
+
+def productDB(M):
+    Xk = np.copy(M)
+    Mk = np.copy(M)
+    I = np.eye(M.shape[0])
+    for i in range(0,30):
+        Xk = (0.5)*(Xk).dot(I + np.linalg.inv(Mk))
+        Mk = (I + ((Mk + np.linalg.inv(Mk))/2))/2
+    return Xk
+
+def CRiteration(M):
+    I = np.eye(M.shape[0])
+    Yk = I - M
+    Zk = 2*(I + M)
+    for i in range(0,30):
+        Yk = ((-Yk.dot(np.linalg.inv(Zk)).dot(Yk)))
+        Zk = Zk + 2 * Yk
+
+    return Zk
+
+def INiteration(M):
+    I = np.eye(M.shape[0])
+    Xk = np.copy(M)
+    Ek = (I - M)/2
+    for i in range(0,30):
+        Xk = Xk + Ek
+        Ek = ((Ek.dot(np.linalg.inv(Xk)).dot(Ek)))/(-2)
+    return Xk
+
+
+
+"""
+[[87, 87, 15, 14, 70],
+[22, 47, 89, 53, 25],
+[62, 16, 82, 97,  6],
+[74, 73, 72,  7,  9],
+[64, 58, 30, 82, 40]]
+"""
+"""
+[[31, 95, 26, 78],
+[25, 29, 86, 29],
+[97, 48, 46, 49],
+[ 7, 27, 45, 45]]
+
+"""
---
+++ schurMethod/handlerFile.py
@@ -0,0 +1,54 @@
+import scipy as sp
+import numpy as np
+import time
+from os.path import expanduser
+import os
+
+workspace = "/Tiro/IOFiles/"
+home = expanduser("~") #home variable contains the user's home folder absolute path
+
+def createWorkSpace():
+    if( not os.path.isdir(home+workspace)):
+        print "Path does not exists"
+        os.makedirs(home+workspace)
+        print 'Don\'t warry I\' ve just created it'
+    else:
+        print "File Folder is OK!"
+
+
+#Get Matrix From File
+def gmff(filename,path=None):
+    if (path is None):
+        return np.loadtxt(home+workspace+filename)
+    elif(path==0):
+        return np.loadtxt(filename)
+        print filenamef
+    else:
+        if(path[len(path)-1]!="/"):
+            path+="/"
+        return np.loadtxt(path+filename)
+    return 0
+
+
+#Print Matrix on File
+def pmof(Matrix,filename=None,path=None):
+    if (filename is None):
+        filename = time.strftime("%y.%m.%d-%H.%M.%S")+".out"
+    if (path is None):
+        path = home+workspace
+    np.savetxt(path+filename,Matrix,fmt='%8.3f') #%17.10f
+    return 0
+
+
+#Generate Random Integer Square Matrix
+def grism(Size):
+    return np.random.randint(0, high=100, size=(Size,Size))
+
+
+#Generate Random Real Square Matrix
+def grrsm(Size):
+    return np.random.uniform(-100000, high=100000, size=(Size,Size))
+
+
+#############TO RUN ##############
+createWorkSpace()
