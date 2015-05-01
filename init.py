549032c51138072e495218b2ad94216b9593006a
sn1p3r46 <kingokongo46@gmail.com>
2015-05-01 02:26:43 +0200

    updatecode

--- init.py
+++ init.py
@@ -1,15 +1,26 @@
 
+from os.path import expanduser
+import os
+
 import scipy as sp
 import scipy.linalg
 
+import time
+
+workspace = "/Tiro/IOFiles/"
+home = expanduser("~")
+
+ttest = "/ttest/"
+ttestBlocks = "/ttestBlocks/"
+ttest2 = "/ttest2/"
 execfile("myHand.py")
 execfile("sqrtm5.py")
 execfile("sqrtm4.py")
 execfile("aggiustala.py")
 A = gmff("BlockedMatrix.out")
 
+"""
 def block_structure(T):
-
     n = len(T)
     tol = 1e-15
     i,j = 0,0
@@ -31,10 +42,198 @@ def block_structure(T):
         bb[j],eb[j] = i,i+1
         j+=1
     return j, bb, eb, s
+"""
 
-B = scipy.random.randint(-100,100,(1200,1200))
+def timeTest(ii):
+    I = np.zeros(ii)
+    F = np.zeros(ii)
+    dims = []
+    Err = []
+    I1 = np.zeros(ii)
+    F1 = np.zeros(ii)
+    dims1 = []
+    Err1 = []
+    #T,Z = sp.linalg.schur(B)
+    #aggiustala(T)
+    Init_Dim = 10
+    for i in range(0,ii):
+        B = scipy.random.randint(-100,100,(Init_Dim,Init_Dim))
+        print "\nCreo matrice B di dimensione: " + str(Init_Dim)+"\n"
+        print "\nOra calcolo schur di B!"
+        T,Z = sp.linalg.schur(B)
+        print "\nDecomposizione di schur terminata con sucesso !!!\n "
+        aggiustala(T)
+        print "\nAggiustata ! ! ! Ora calcolo sqrtm5(T)\n"
+        I[i]=time.clock()
+        MM = sqrtm5(T)
+        F[i]=time.clock()
+        print "\nCalcolata! Ora calcolo sp.linalg.sqrtm(T)\n"
+        Err.append(sp.linalg.norm(MM.dot(MM)-T)/sp.linalg.norm(T))
+        dims.append(Init_Dim)
 
-T,Z = sp.linalg.schur(B)
+        I1[i]=time.clock()
+        MM = sp.linalg.sqrtm(T)
+        F1[i]=time.clock()
+        Err1.append(sp.linalg.norm(MM.dot(MM)-T)/sp.linalg.norm(T))
+        dims1.append(Init_Dim)
+        print "\nFinita dimensione--------------------" + str(Init_Dim) +"\n"
+        Init_Dim=Init_Dim*2
 
-aggiustala(T)
-print min(sp.linalg.eigvals(T))
+
+
+    np.savetxt(home + workspace + ttest + "TimeStart_"+str(ii)+".tt", I, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' inizio del calcolo per sqrtm5()')
+    np.savetxt(home + workspace + ttest + "TimeEnd_"+str(ii)+".tt", F, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' fine del calcolo per sqrtm5()')
+    np.savetxt(home + workspace + ttest + "DeltaTime_"+str(ii)+".tt", (F-I), fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' Tempo realmente impiegato da sqrtm5,\n il numero ii indica la dim della matrice come 10*2^ii con ii che va da 0 a ii')
+    np.savetxt(home + workspace + ttest + "dimensionematricigenerate_"+str(ii)+".tt", dims, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' dim delle matrici su cui sono effettuati i calcoli')
+    np.savetxt(home + workspace + ttest + "ErrRelativo_"+str(ii)+".tt", Err, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' Errore Relativo Per ogni matrice')
+
+
+    np.savetxt(home + workspace + ttest + "TimeStart1_"+str(ii)+".tt", I1, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' inizio del calcolo per sp.linalg.sqrtm()')
+    np.savetxt(home + workspace + ttest + "TimeEnd1_"+str(ii)+".tt", F1, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' fine del calcolo per sp.linalg.sqrtm()')
+    np.savetxt(home + workspace + ttest + "DeltaTime1_"+str(ii)+".tt", (F1-I1), fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' Tempo realmente impiegato da sp.linalg.sqrtm(),\n il numero ii indica la dim della matrice come 10*2^ii con ii che va da 0 a ii')
+    np.savetxt(home + workspace + ttest + "dimensionematricigenerate1_"+str(ii)+".tt", dims1, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' dim delle matrici su cui sono effettuati i calcoli')
+    np.savetxt(home + workspace + ttest + "ErrRelativo1_"+str(ii)+".tt", Err1, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' Errore Relativo Per ogni matrice')
+
+
+    print "I:"
+    print I
+    print "F:"
+    print F
+    print "F - I:"
+    print F - I
+    print "Dims:"
+    print dims
+    print "I1:"
+    print I1
+    print "F1:"
+    print F1
+    print "F1 - I1:"
+    print F1 - I1
+    print "Dims:"
+    print dims1
+    print "ASSOLUTO"
+    print Err
+    print Err1
+
+def timeTest1(ii,jj):
+    I = np.zeros((ii,jj-2))
+    F = np.zeros((ii,jj-2))
+    print I.shape
+    dims = []
+    #T,Z = sp.linalg.schur(B)
+    #aggiustala(T)
+    Init_Dim = 10
+    for i in range(0,ii):
+        B = scipy.random.randint(-100,100,(Init_Dim,Init_Dim))
+        T,Z = sp.linalg.schur(B)
+        aggiustala(T)
+        for j in range(2,jj):
+            I[i,j-2] = time.clock()
+            MM   = sqrtm5(T,j)
+            F[i,j-2] = time.clock()
+	dims.append(Init_Dim)
+	Init_Dim=Init_Dim*2
+
+    FI = F - I
+
+# il seguente output e' incentrato a verificare se ci sono cambiamenti a livello di tempo al variare delle dimensioni dei blocchi grandi insiame alle matrici
+# ogni colonna contiene il tempo al variare dei blocchi sulla matrice,
+# mentre ogni riga aumenta la dimensione della matrice in ordine quadratico;
+# min e max time  invece misurano il massimo cambiamento di tempo per ogni riga, per vedere con deltatime poi se effettivamente la dimensione dei blocchi conta ai fini della velocita.
+
+    np.savetxt(home + workspace + ttestBlocks + "TimeStart"+str(ii)+".tt", I, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' inizio del calcolo per sqrtm5()')
+    np.savetxt(home + workspace + ttestBlocks + "TimeEnd"+str(ii)+".tt", F, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' fine del calcolo per sqrtm5()')
+    np.savetxt(home + workspace + ttestBlocks + "DeltaTime"+str(ii)+".tt", (F-I), fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' Tempo realmente impiegato da sqrtm5,\n il numero ii indica la dim della matrice come 10*2^ii con ii che va da 0 a ii')
+    np.savetxt(home + workspace + ttestBlocks + "dimensionematricigenerate"+str(ii)+".tt", dims, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' dim delle matrici su cui sono effettuati i calcoli')
+#   np.savetxt(home + workspace + ttestBlocks + "ErrRelativo"+str(ii)+".tt", Err, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' Errore Relativo Per ogni matrice')
+    np.savetxt(home + workspace + ttestBlocks + "MaxTime" +str(ii)+".tt", np.amax(FI, axis=1), fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' tempo massimo al variare delle dims dei blocchi per ogni matrice')
+    np.savetxt(home + workspace + ttestBlocks + "MinTime" +str(ii)+".tt", np.amin(FI, axis=1), fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' tempo minimo al variare delle dimens dei blocchi per ogni matrice')
+    np.savetxt(home + workspace + ttestBlocks + "DeltaTime"+ str(ii)+".tt", (np.amax(FI, axis=1) - np.amin(FI, axis=1)) , fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' differenza tra il tempo massimo ed il tempo minimo per ogni matrice')
+
+    print "I:"
+    print I
+    print "F:"
+    print F
+    print "F-I:"
+    print F - I
+    print "dims:"
+    print dims
+    print "MAX:"
+    print np.amax(FI, axis=1)
+    print "MIN:"
+    print np.amin(FI, axis=1)
+    print "DIFF:"
+    print np.amax(FI, axis=1) - np.amin(FI, axis=1)
+
+
+def timeTest2(ii=1,dimensione=10240):
+    I = np.zeros(ii)
+    F = np.zeros(ii)
+    dims = []
+    Err = []
+    I1 = np.zeros(ii)
+    F1 = np.zeros(ii)
+    dims1 = []
+    Err1 = []
+    #T,Z = sp.linalg.schur(B)
+    #aggiustala(T)
+
+    for i in range(ii-1,ii):
+        B = scipy.random.rand(dimensione,dimensione)
+        print "\nCreo matrice B di dimensione: " + str(dimensione)+"\n"
+	print "\nOra calcolo schur di B!\n"
+        T,Z = sp.linalg.schur(B)
+	del B
+	print "\nDecomposizione di schur terminata con sucesso !!! AGGIUSTO\n "
+        aggiustala(T)
+        print "\nAggiustata ! ! ! Ora calcolo sqrtm5(T)\n"
+        I[i]=time.clock()
+        MM = sqrtm5(T)
+        F[i]=time.clock()
+        print "\nCalcolata! Ora calcolo sp.linalg.sqrtm(T)\n"
+        Err.append(sp.linalg.norm(MM.dot(MM)-T)/sp.linalg.norm(T))
+        dims = [dimensione]
+        I1[i]=time.clock()
+        MM = sp.linalg.sqrtm(T)
+        F1[i]=time.clock()
+        Err1.append(sp.linalg.norm(MM.dot(MM)-T)/sp.linalg.norm(T))
+        dims1 = [dimensione]
+	print "\nFinita dimensione--------------------" + str(dimensione) +"\n"
+
+
+
+        np.savetxt(home + workspace + ttest2 + "00TimeStart_"+str(ii)+".tt", I, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' inizio del calcolo per sqrtm5()')
+        np.savetxt(home + workspace + ttest2 + "00TimeEnd_"+str(ii)+".tt", F, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' fine del calcolo per sqrtm5()')
+        np.savetxt(home + workspace + ttest2 + "00DeltaTime_"+str(ii)+".tt", (F-I), fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' Tempo realmente impiegato da sqrtm5,\n il numero ii indica la dim della matrice come 10*2^ii con ii che va da 0 a ii')
+        np.savetxt(home + workspace + ttest2 + "00dimensionematricigenerate_"+str(ii)+".tt", dims, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' dim delle matrici su cui sono effettuati i calcoli')
+        np.savetxt(home + workspace + ttest2 + "00ErrASSOLUTO"+str(ii)+".tt", Err, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' Errore Relativo Per ogni matrice')
+
+
+        np.savetxt(home + workspace + ttest2 + "00TimeStart1_"+str(ii)+".tt", I1, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' inizio del calcolo per sp.linalg.sqrtm()')
+        np.savetxt(home + workspace + ttest2 + "00TimeEnd1_"+str(ii)+".tt", F1, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' fine del calcolo per sp.linalg.sqrtm()')
+        np.savetxt(home + workspace + ttest2 + "00DeltaTime1_"+str(ii)+".tt", (F1-I1), fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' Tempo realmente impiegato da sp.linalg.sqrtm(),\n il numero ii indica la dim della matrice come 10*2^ii con ii che va da 0 a ii')
+        np.savetxt(home + workspace + ttest2 + "00dimensionematricigenerate1_"+str(ii)+".tt", dims1, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' dim delle matrici su cui sono effettuati i calcoli')
+        np.savetxt(home + workspace + ttest2 + "00ErrASSOLUTO_"+str(ii)+".tt", Err1, fmt='%.18e', delimiter=' ', newline='\n', header='', footer='', comments=' Errore Relativo Per ogni matrice')
+
+
+        print "I:"
+        print I
+        print "F:"
+        print F
+        print "F - I:"
+        print F - I
+        print "Dims:"
+        print dims
+        print "I1:"
+        print I1
+        print "F1:"
+        print F1
+        print "F1 - I1:"
+        print F1 - I1
+        print "Dims:"
+        print dims1
+        print "ASSOLUTO"
+        print Err
+        print Err1
+
+#print min(sp.linalg.eigvals(T))
--- schurMethod/sqrtm2BIS.py
+++ schurMethod/sqrtm2BIS.py
@@ -1,45 +1,61 @@
 import numpy as np
 import scipy as sp
 from scipy import linalg
-
 from scipy.linalg.lapack import ztrsyl, dtrsyl
+from os.path import expanduser
+import os
+import scipy as sp
+import scipy.linalg
+import time
 
 
-def sqrtm2(M):
-    m, fb, fe, s = block_structure(M)
+
+execfile("myHand.py")
+execfile("aggiustala.py")
+
+def realSchurNonBlocked(M):
+    m, fb, fe, s = block_structure(M) #computes the block Structure
     n = M.shape[0]
     ris = sp.zeros((n,n))
     for i in range(0,m):
-        ris[fb[i]:fe[i],fb[i]:fe[i]] = twobytworoot(M[fb[i]:fe[i],fb[i]:fe[i]])
-        
+        ris[fb[i]:fe[i],fb[i]:fe[i]] = tbtroot(M[fb[i]:fe[i],fb[i]:fe[i]])
+
     for j in range(1,m):
         for i in range(0,m-j):
             Tnoto = sp.copy(M[fb[i]:fe[i],fb[i+j]:fe[i+j]]) #dopo togliere il copy
+
             for k in range(i+1,i+j):
                 Tnoto -= (ris[fb[i]:fe[i],fb[k]:fe[k]]).dot(ris[fb[k]:fe[k],fb[j+i]:fe[j+i]])
+
             if((M[fb[i]:fe[i],fb[i+j]:fe[i+j]]).shape==(1,1)):
                 ris[fb[i]:fe[i],fb[i+j]:fe[i+j]] = Tnoto/(ris[fb[i]:fe[i],fb[i]:fe[i]] + ris[fb[i+j]:fe[i+j],fb[i+j]:fe[i+j]])
+
             else:
                 Uii = ris[fb[i]:fe[i],fb[i]:fe[i]]
                 Ujj = ris[fb[i+j]:fe[i+j],fb[i+j]:fe[i+j]]
                 shapeUii = Uii.shape[0]
                 shapeUjj = Ujj.shape[0]
-                x, scale, info = dtrsyl(Uii, Ujj, Tnoto)
+                x, scale, info = dtrsyl(Uii, Ujj, Tnoto) #solve the slvester equation
                 ris[fb[i]:fe[i],fb[i+j]:fe[i+j]] = x * scale
-
     return ris
 
-"""
-            else:
-                print "formaX"
-                print M[fb[i]:fe[i],fb[i+j]:fe[i+j]]
-                print M[fb[i]:fe[i],fb[i]:fe[i]]
-                print M[fb[i+j]:fe[i+j],fb[i+j]:fe[i+j]]
-    return ris
-"""
 
-def twobytworoot(matrix):
+def csr2(complex_n):
+    t = sp.sqrt((sp.absolute(complex_n.real)+(sp.sqrt(sp.square(complex_n.real)+sp.square(complex_n.imag))))/2)
+    if(complex_n.real >= 0):
+        return complex(t,(complex_n.imag/(2*t)))
+    else:
+        return complex((complex_n.imag/(2*t)),t)
+
+def gev(matrix):
+    x = (matrix[0,0]+matrix[1,1])/2
+    y = sp.sqrt(-sp.square(matrix[0,0]-matrix[1,1])-4*matrix[1,0]*matrix[0,1])/2
+    return complex(x,y)
+
+def tbtroot(matrix):
     if (matrix.shape[0]==2):
+        a = gev(matrix)
+        a = csr2(a).real
         a = sp.sqrt(sp.linalg.eigvals(matrix)[0]).real
         ris=sp.ndarray(shape=(2,2))
         ris[0,0] = a + (1/(4*a))*(matrix[0,0] - matrix[1,1])
@@ -62,11 +78,9 @@ def block_structure(T):
 
     n = len(T)
     tol = 1e-15
-
     i = 0
     v = []
     s = []
-
     while i < n-1:
         v.append(i)
         if abs(T[i+1,i])<tol:
@@ -85,3 +99,16 @@ def block_structure(T):
     fe = [v[i] + s[i] for i in range(m)]
 
     return m, fb, fe, s
+
+
+
+B = scipy.random.randint(-100,100,(10,10))
+print "\nCreo matrice B"
+print "\nOra calcolo schur di B!"
+T,Z = sp.linalg.schur(B)
+print "\nDecomposizione di schur terminata con sucesso !!!\n "
+aggiustala(T)
+
+DD = realSchurNonBlocked(T)
+
+print sp.linalg.norm(DD.dot(DD)-T)/sp.linalg.norm(T)
